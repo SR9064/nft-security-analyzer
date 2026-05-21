317216308ca290251f9ded16b436f21c0ac22737
SR9064 <sahelim481@gmail.com>
2026-05-21 17:51:44 +0000

    Remove font import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-import "@fontsource/inter"
 
 import "./index.css"
 
@@ -16,7 +15,5 @@ ReactDOM.createRoot(
   document.getElementById("root")!
 ).render(
 
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 )
